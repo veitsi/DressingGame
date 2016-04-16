e2b4efa989bbaaaa6323b6da77430ee555773369
pydev <xtfkpi@gmail.com>
2016-04-16 12:52:12 +0300

    we have index of old sample

--- brain.py
+++ brain.py
@@ -2,6 +2,13 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.structure import LinearLayer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
+import numpy
+
+def sindex(ds,arr):
+    for i in range(len(ds['input'])):
+        if numpy.array_equal(ds['input'][i],arr):
+            return i
+    return -1
 
 # r=[]
 # for i in range(300):
@@ -14,12 +21,17 @@ net = buildNetwork(6, 4, 1, outclass=LinearLayer)
 ds = SupervisedDataSet(6, 1)
 ds.addSample((0, 0, 1, 0, 3, 1), (0,))
 ds.addSample((4, 1, 3, 0, 2, 1), (1,))
+ds.addSample((4, 1, 3, 0, 2, 1), (0,))
+
+print(len(ds))
+
+print(sindex(ds,[4, 1, 3, 0, 2, 1]))
 
 print(ds['input'][0])
-ds['input'][0][1]=1
+ds['input'][0][1] = 1
 print(ds['input'][0])
 
 trainer = BackpropTrainer(net, ds)
 trainer.trainUntilConvergence()
 
-print(net.activate([0, 0, 1, 0, 3, 1]))
+print(net.activate([4, 1, 3, 0, 2, 1]))
--- gui.py
+++ gui.py
@@ -7,21 +7,24 @@ root.geometry('300x200')
 
 colors = ["white","yellow","pink", "green","black"]
 
-def button_clicked(num):
-    print("Hello "+str(num))
+def button_clicked(signals):
+    print("Hello "+str(signals))
     print(button1['text'],button2['text'],button3['text'])
 
 
 def b1():
-    button_clicked(1)
+    button_clicked([0,1,1])
     # button1['text']='2'
     # button1['bg']="green"
 
 def b2():
-    button_clicked(2)
+    button_clicked([1,0,1])
 
 def b3():
-    button_clicked(3)
+    button_clicked([1,1,0])
+
+def burn():
+    button_clicked([0,0,0])
 
 def close():
     root.destroy()
@@ -30,9 +33,11 @@ def close():
 button1 = Button(root, text=colors[random.randrange(0,5)], command=b1)
 button2 = Button(root, text=colors[random.randrange(0,5)], command=b2)
 button3 = Button(root, text=colors[random.randrange(0,5)], command=b3)
+button = Button(root, text="Burn it all!", command=burn)
 button1.pack(fill=BOTH)
 button2.pack(fill=BOTH)
 button3.pack(fill=BOTH)
+button.pack(fill=BOTH)
 
 root.protocol('WM_DELETE_WINDOW', close)
 
